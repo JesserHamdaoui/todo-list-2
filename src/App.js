e8b4c7ea84d99bc3e62bb6aebb9c68c2ed1a12b8
JesserHamdaoui <hamdaouijesser2004@gmail.com>
2023-08-12 22:43:27 +0100

    START ADDING THE EDIT FUNCTIONALITY
    Add an isEditing variable to each task in the tasks array
    Add an event listener to the edit button which trigger the handleEdit function
    Add the handleEdit function which update the isEditing state of the targeted task !!STILL NOT FUNCTIONING
    Add the layout of the task for editing

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import { NewTask, TaskList } from "./components";
 
 function App() {
   const [tasks, setTasks] = useState([
-    { key: 1, done: false, text: "Buy groceries" },
-    { key: 2, done: true, text: "Clean the house" },
-    { key: 3, done: false, text: "Finish React project" },
-    { key: 4, done: true, text: "Go for a walk" },
-    { key: 5, done: false, text: "Read a book" },
+    { key: 1, done: false, text: "Buy groceries", isEditing: false },
+    { key: 2, done: true, text: "Clean the house", isEditing: false },
+    { key: 3, done: false, text: "Finish React project", isEditing: false },
+    { key: 4, done: true, text: "Go for a walk", isEditing: false },
+    { key: 5, done: false, text: "Read a book", isEditing: false },
   ]);
 
   return (
--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -2,6 +2,11 @@ import './task-list.css'
 
 const TaskList = ({tasks, setTasks}) => {
 
+    const handleEdit = (key) => {
+        console.log(key)
+        setTasks(tasks.map(task => (task.id === key ? {...tasks, isEditing: !task.isEditing} : task ))
+    )}
+
     const handleDelete = (key) => {
         setTasks(tasks.filter(task => {
             return(
@@ -13,16 +18,27 @@ const TaskList = ({tasks, setTasks}) => {
     return ( 
         <div>
             <ul>
+                {console.log(tasks)}
                 {tasks.map(task => {
-                    return(
-                    <li key={task.key}>
-                        <p className={task.done? 'done':''}>{task.text}</p>
-                        <div className="btns">
-                            <p>edit</p>
-                            <p onClick={() => handleDelete(task.key)}>delete</p>
-                        </div>
-                    </li>   
-                    )
+                    if(task.isEditing) {
+                        return(
+                            <div>
+                                <input type="text" />
+                                <button>OK</button>
+                            </div>
+                        )
+                    }
+                    else {
+                        return(
+                            <li key={task.key}>
+                                <p className={task.done? 'done':''}>{task.text}</p>
+                                <div className="btns">
+                                    <p onClick={() => handleEdit(task.key)}>edit</p>
+                                    <p onClick={() => handleDelete(task.key)}>delete</p>
+                                </div>
+                            </li>   
+                        )
+                    }
                 })}
             </ul>
         </div>
