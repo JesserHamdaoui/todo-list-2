f37f38b97aad6a6b70e1489bea689dcbf038c1c5
JesserHamdaoui <hamdaouijesser2004@gmail.com>
2023-08-14 19:29:47 +0100

    ADD A LOCAL STORAGE FOR TASKS
    Get the tasks array from the local storage and make it as the initial value of the tasks variable
    Update the local storage whenever a change happen to the tasks variable

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { NewTask, TaskList } from "./components";
 
 function App() {
-  const [tasks, setTasks] = useState([
-    { key: 1, done: false, text: "Buy groceries", isEditing: false },
-    { key: 2, done: true, text: "Clean the house", isEditing: false },
-    { key: 3, done: false, text: "Finish React project", isEditing: false },
-    { key: 4, done: true, text: "Go for a walk", isEditing: false },
-    { key: 5, done: false, text: "Read a book", isEditing: false },
-  ]);
+  const [tasks, setTasks] = useState(JSON.parse(localStorage.getItem("tasks")));
+
+  useEffect(
+    () => localStorage.setItem("tasks", JSON.stringify(tasks)),
+    [tasks]
+  );
 
   return (
     <div className="App">
